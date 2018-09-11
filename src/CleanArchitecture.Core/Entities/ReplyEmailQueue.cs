019847e89a8c7ac5b1ade57d87cca606dba0df10
khushali_JBSPL <khushali@jbspl.com>
2018-09-11 17:30:57 +0530

    Added enitiy for  SMS and Email , craete Request class for sms API

---
+++ src/CleanArchitecture.Core/Entities/ReplyEmailQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class ReplyEmailQueue
+    {
+        [Key]
+        public long EmailID { get; set; }
+
+        public long RefNo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Recepient { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+
+        [StringLength(500)]
+        public string CC { get; set; }
+
+        [StringLength(500)]
+        public string BCC { get; set; }
+
+        [StringLength(500)]
+        public string Attachment { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public short Status { get; set; }
+
+        public short SendBy { get; set; }
+
+        public short EmailType { get; set; }
+
+        public short ChannelID { get; set; }
+
+        public DateTime SendDate { get; set; }
+    }
+}
