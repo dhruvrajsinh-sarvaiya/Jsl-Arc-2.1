37172bcf4a0fdcc1c9dcdbb3a26c231b48416597
khushali_JBSPL <khushali@jbspl.com>
2018-09-12 18:27:56 +0530

    enum added
    common enum

---
+++ src/CleanArchitecture.Core/Enums/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.Enums
+{
+    public enum ServiceStatus
+    {
+        Disable = 9,
+        Enable = 1        
+    }
+}
