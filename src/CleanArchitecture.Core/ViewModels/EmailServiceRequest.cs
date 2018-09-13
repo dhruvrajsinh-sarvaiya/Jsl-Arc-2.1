72fb4f276fd9d86124c23c8bf469c6ad9b38fd71
khushali_JBSPL <khushali@jbspl.com>
2018-09-13 10:54:36 +0530

    Added request for SMS and Email
    ViewModels

---
+++ src/CleanArchitecture.Core/ViewModels/EmailServiceRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels
+{
+    public class EmailServiceRequest
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
+        public string Message { get; set; }
+    }
+}
---
+++ src/CleanArchitecture.Core/ViewModels/SMSServiceRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels
+{
+    public class SMSServiceRequest
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Phone]
+        public long MobileNo { get; set; }
+
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
+        public string Message { get; set; }
+    }
+}
