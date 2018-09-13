6e23b3048a81ded0c25f43d8f74696c72660008c
khushali_JBSPL <khushali@jbspl.com>
2018-09-13 19:46:46 +0530

    solve implementation error

--- src/CleanArchitecture.Core/Services/MessageService.cs
+++ src/CleanArchitecture.Core/Services/MessageService.cs
@@ -8,17 +8,22 @@ namespace CleanArchitecture.Core.Services
 {
     public class MessageService : IMessageService
     {
-        public Task SendMessageAsync(string Email, string Recepient, string Subject, string BCC, string CC, string Body, string Url, string UserID, string Password, string Port)
+        public Task SendEmailViaSendgridAsync(string Email, string Recepient, string Subject, string BCC, string CC, string Body, string Url, string UserID, string Password)
         {
             return Task.FromResult(0);
         }
 
-        public Task SendMessageAsync(string Email, string Recepient, string Subject, string BCC, string CC, string Body, string Url, string UserID, string Password)
+        public Task SendEmailViaSMTPAsync(string Email, string Recepient, string Subject, string BCC, string CC, string Body, string Url, string UserID, string Password, string Port)
         {
             return Task.FromResult(0);
         }
 
-        public Task SendMessageAsync(string Mobile, string Message, string Url, string UserID, string Password)
+        public Task SendMessageAsync(string Mobile, string Message, string Url, string SerderID, string UserID, string Password)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendNotificationAsync(string DeviceID, string Message, string Url)
         {
             return Task.FromResult(0);
         }
