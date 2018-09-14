48f14ed10f7a312fd86e77eb4cd45f672d847277
RitaChavda <rita@jbspl.com>
2018-09-14 12:47:52 +0530

    added new enum
    for transaction enum

--- src/CleanArchitecture.Core/Enums/Enums.cs
+++ src/CleanArchitecture.Core/Enums/Enums.cs
@@ -10,4 +10,23 @@ namespace CleanArchitecture.Core.Enums
         Active = 1,
         InActive = 0
     }
+    public enum TransactionStatus
+    {
+        Success = 1,
+        OperatorFail = 2 ,
+        SystemFail = 3,
+        Hold = 4,
+        Refunded = 5,
+        Pending = 6
+    }
+    public enum EnTrnType
+    {       
+        Transaction = 1,
+        Buy_Trade = 4,
+        Sell_Trade = 5,
+        Withdraw = 6,
+        Shoping_Cart = 7,
+        Deposit = 8,
+        Generate_Address = 9
+    }
 }
