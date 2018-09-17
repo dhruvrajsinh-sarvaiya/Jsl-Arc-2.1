ab21b1981c8ef008f99da8960338f1d4f8a822ea
khushali_JBSPL <khushali@jbspl.com>
2018-09-17 18:30:47 +0530

    Move core service to infrastructure

---
+++ src/CleanArchitecture.Infrastructure/Services/MessageService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Services
+{
+    public class MessageService : IMessageService
+    {
+        public Task SendEmailAsync(string Email, string Recepient, string Subject, string BCC, string CC, string Body, string Url, string UserID, string Password, string Port)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendSMSAsync(string Mobile, string Message, string Url, string SerderID, string UserID, string Password)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SendNotificationAsync(string DeviceID, string Message, string Url)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
