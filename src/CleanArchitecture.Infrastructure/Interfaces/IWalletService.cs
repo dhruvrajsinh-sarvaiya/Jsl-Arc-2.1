921f424511dbafb74ad84361f2bc31f887cfcb4a
Nupoora <nupoora@jbspl.com>
2018-09-19 17:30:47 +0530

    interface moved from core to infrastructure
    interface moved from core to infrastructure

---
+++ src/CleanArchitecture.Infrastructure/Interfaces/IWalletService.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Interfaces
+{
+    
+    public interface IWalletService
+    {
+        decimal GetUserBalance(int walletId);
+
+        bool CreditWallet(int walletId, ref decimal PostBal);
+
+        bool WalletBalanceCheck(decimal amount,long walletId);
+
+        bool DebitWallet(int walletId,ref decimal PostBal);
+
+    }
+}
