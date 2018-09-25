06c0c63c97a13d64fd1e0c9a692b785b75f2dc6a
RitaChavda <rita@jbspl.com>
2018-09-25 19:23:43 +0530

    Make transaction
    Make transaction class

---
+++ CleanArchitecture.Infrastructure/Services/MakeTransaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.Entities;
+
+namespace CleanArchitecture.Infrastructure.Services
+{
+    public class MakeTransaction
+    {
+        private readonly IRepository<TransactionQueue> _TransactionRepository;
+
+        public MakeTransaction(IRepository<TransactionQueue> TransactionRepository)
+        {
+            _TransactionRepository = TransactionRepository;
+        }
+
+        public string CreateTransaction(TransactionQueue NewtransactionReq)
+        {
+            var Newtransaction = new TransactionQueue()
+            {
+                TrnDate = DateTime.UtcNow,
+                TrnMode = NewtransactionReq.TrnMode,
+                TrnType = NewtransactionReq.TrnType,
+                MemberID = NewtransactionReq.MemberID,
+                MemberMobile = NewtransactionReq.MemberMobile,
+                SMSText = NewtransactionReq.SMSText,
+                SMSCode = NewtransactionReq.SMSCode,
+                Amount = NewtransactionReq.Amount,
+                SMSPwd = NewtransactionReq.SMSPwd,
+                ServiceID = NewtransactionReq.ServiceID,
+                SerProID = NewtransactionReq.SerProID,
+                ProductID = NewtransactionReq.ProductID,
+                RoutID = NewtransactionReq.RoutID,
+                Status = NewtransactionReq.Status,
+                StatusCode  = NewtransactionReq.StatusCode,
+                StatusMsg = NewtransactionReq.StatusMsg,
+                VerifyDone =0,
+                TrnRefNo = NewtransactionReq.TrnRefNo,
+                ChargePer = NewtransactionReq.ChargePer,
+                ChargeRs = NewtransactionReq.ChargeRs,
+                ChargeType = NewtransactionReq.ChargeType,
+            };
+            _TransactionRepository.Add(Newtransaction);
+            return "Success";
+        }
+    }
+}
