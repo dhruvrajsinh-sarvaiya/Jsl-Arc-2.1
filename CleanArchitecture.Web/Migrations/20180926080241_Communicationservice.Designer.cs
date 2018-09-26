3732f260c31aca196d5c07ca6246f0a5e610f6c2
khushali_JBSPL <khushali@jbspl.com>
2018-09-26 15:07:09 +0530

    Communictaion migration

--- CleanArchitecture.Web/Migrations/20180926080241_Communicationservice.Designer.cs
+++ CleanArchitecture.Web/Migrations/20180926080241_Communicationservice.Designer.cs
@@ -4,14 +4,16 @@ using CleanArchitecture.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArchitecture.Web.Migrations
 {
     [DbContext(typeof(CleanArchitectureContext))]
-    partial class CleanArchitectureContextModelSnapshot : ModelSnapshot
+    [Migration("20180926080241_Communicationservice")]
+    partial class Communicationservice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
