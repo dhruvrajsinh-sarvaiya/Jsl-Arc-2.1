1e37982bcba9fa21a1588d4b5e389aadba65a4e9
birjujbspl <birju@jbspl.com>
2018-09-28 09:42:07 +0530

    Rename Model Name

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/SignUpWithEmailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Core.ApiModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Core.ViewModels.AccountViewModels
+{
+    public class SignUpWithEmailViewModel : TrackerViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class SignUpWithEmailResponse : BizResponseClass
+    {
+
+    }
+}
---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/SignUpWithMobileViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Core.ApiModels;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Core.ViewModels.AccountViewModels
+{
+    public class SignUpWithMobileViewModel : TrackerViewModel
+    {
+        [Required]        
+        [StringLength(10, MinimumLength = 10)]
+        [Range(10, Int64.MaxValue)]
+        public string Mobile { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class SignUpMobileWithOTPResponse : BizResponseClass
+    {
+
+    }
+}
