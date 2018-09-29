c60ab05f69ad40d4ef9744d5f2e293a469e5d1aa
khushali_JBSPL <khushali@jbspl.com>
2018-09-29 13:12:45 +0530

    Revert "Test Conflict"
    This reverts commit 309c3e7be5d78ec63ad30ab49f5c80e72b8b2ae9.

--- CleanArchitecture.Infrastructure/Services/WebApiParseResponse.cs
+++ CleanArchitecture.Infrastructure/Services/WebApiParseResponse.cs
@@ -11,7 +11,7 @@ namespace CleanArchitecture.Infrastructure.Services
     {
         public WebAPIParseResponse TransactionParseResponse(string TransactionResponse, long ThirPartyAPIID)
         {
-            string test = "Test conflict";
+            string test = "";
             throw new NotImplementedException();
         }
     }
