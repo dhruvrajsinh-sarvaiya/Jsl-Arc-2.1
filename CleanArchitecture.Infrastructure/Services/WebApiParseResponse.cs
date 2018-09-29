e5a850209749c9b64c23df69a445f72362add5dd
RitaChavda <rita@jbspl.com>
2018-09-29 13:23:35 +0530

    test conflict

--- CleanArchitecture.Infrastructure/Services/WebApiParseResponse.cs
+++ CleanArchitecture.Infrastructure/Services/WebApiParseResponse.cs
@@ -11,7 +11,7 @@ namespace CleanArchitecture.Infrastructure.Services
     {
         public WebAPIParseResponse TransactionParseResponse(string TransactionResponse, long ThirPartyAPIID)
         {
-            string test = "test conflict";
+            string test = "test conflictpo";
             throw new NotImplementedException();
         }
     }
