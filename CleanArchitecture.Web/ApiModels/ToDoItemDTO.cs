b0d1d42d8cb76fade84c1b1091faeef22b1e1ffe
vishvata <vishvata@jbspl.com>
2018-09-29 17:21:15 +0530

    Test Commit 2

--- CleanArchitecture.Web/ApiModels/ToDoItemDTO.cs
+++ CleanArchitecture.Web/ApiModels/ToDoItemDTO.cs
@@ -7,7 +7,6 @@ namespace CleanArchitecture.Web.ApiModels
     public class ToDoItemDTO
     {
         public int Id { get; set; }
-
         [Required]
         public string Title { get; set; }
         public string Description { get; set; }
