9b7e6d0dde78b5465c91c7b5f8c7d4d544eaab1c
Nupoora <nupoora@jbspl.com>
2018-10-02 18:53:36 +0530

    response class added
    response class added

---
+++ CleanArchitecture.Infrastructure/DTOClasses/BizResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArchitecture.Core.Enums;
+
+namespace CleanArchitecture.Infrastructure.DTOClasses
+{
+    public class BizResponse
+    {
+        public byte StatusCode { get; set; }
+        public enErrorCode ErrorCode { get; set; }
+        public string ReturnMsg { get; set; }
+    }
+}
