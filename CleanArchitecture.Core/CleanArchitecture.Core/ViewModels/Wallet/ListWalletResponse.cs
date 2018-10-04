ad76a828e9854bf78e4f99af02f720b8010b1531
vishvata <vishvata@jbspl.com>
2018-10-04 14:56:38 +0530

    Craete common class for response

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/ListWalletResponse.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/ListWalletResponse.cs
@@ -1,45 +1,50 @@
-﻿using System;
+﻿using CleanArchitecture.Core.ApiModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CleanArchitecture.Core.ViewModels.Wallet
 {
-    public class ListWalletResponse
+    public class ListWalletResponse : BizResponseClass
     {
-        public class User
-        {
-            public string user { get; set; }
-            public List<string> permissions { get; set; }
-        }
-
-        public class Freeze
-        {
-        }
-
-        public class CoinSpecific
-        {
-        }
-
-        public class Wallet
-        {
-            public string id { get; set; }
-            public List<User> users { get; set; }
-            public string coin { get; set; }
-            public string label { get; set; }
-            public int m { get; set; }
-            public int n { get; set; }
-            public List<string> keys { get; set; }
-            public List<string> tags { get; set; }
-            public bool disableTransactionNotifications { get; set; }
-            public Freeze freeze { get; set; }
-            public bool deleted { get; set; }
-            public int approvalsRequired { get; set; }
-            public CoinSpecific coinSpecific { get; set; }
-        }
-
-        public class ListWalletRootObject : ApiModels.BizResponseClass
-        {
-            public List<Wallet> wallets { get; set; }
-        }
+        public List<Wallet> wallets { get; set; }
     }
+    //public class ListWalletResponse
+    //{
+    //    public class User
+    //    {
+    //        public string user { get; set; }
+    //        public List<string> permissions { get; set; }
+    //    }
+
+    //    public class Freeze
+    //    {
+    //    }
+
+    //    public class CoinSpecific
+    //    {
+    //    }
+
+    //    public class Wallet
+    //    {
+    //        public string id { get; set; }
+    //        public List<User> users { get; set; }
+    //        public string coin { get; set; }
+    //        public string label { get; set; }
+    //        public int m { get; set; }
+    //        public int n { get; set; }
+    //        public List<string> keys { get; set; }
+    //        public List<string> tags { get; set; }
+    //        public bool disableTransactionNotifications { get; set; }
+    //        public Freeze freeze { get; set; }
+    //        public bool deleted { get; set; }
+    //        public int approvalsRequired { get; set; }
+    //        public CoinSpecific coinSpecific { get; set; }
+    //    }
+
+    //    public class ListWalletRootObject : ApiModels.BizResponseClass
+    //    {
+    //        public List<Wallet> wallets { get; set; }
+    //    }
+    //}
 }
