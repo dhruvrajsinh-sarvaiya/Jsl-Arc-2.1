c2143b2afd97c6fce07544fa8f0c8dbb73640ea6
komal siddhapura <komal@jbspl.com>
2018-10-05 15:56:00 +0530

    Add Service and Repository Interface and class for Front Transaction Methods

---
+++ CleanArchitecture.Infrastructure/Data/Transaction/FrontTrnRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Core.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Data.Transaction
+{
+    public class FrontTrnRepository : IFrontTrnRepository
+    {
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Services/Transaction/FrontTrnService.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Services.Transaction
+{
+    public class FrontTrnService : IFrontTrnService
+    {
+    }
+}
