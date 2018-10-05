67cec2d66a9a41a97d82f3893c90932d2740ccff
Uday Dodiya <uday@jbspl.com>
2018-10-05 17:03:15 +0530

    add a contructor for FrontTrnRepository and FrontTrnService

--- CleanArchitecture.Infrastructure/Data/Transaction/FrontTrnRepository.cs
+++ CleanArchitecture.Infrastructure/Data/Transaction/FrontTrnRepository.cs
@@ -1,11 +1,32 @@
 ﻿using CleanArchitecture.Core.Interfaces.Repository;
+using CleanArchitecture.Core.ViewModels.Transaction;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CleanArchitecture.Infrastructure.Data.Transaction
 {
     public class FrontTrnRepository : IFrontTrnRepository
     {
+        private readonly CleanArchitectureContext _dbContext;
+
+        public FrontTrnRepository(CleanArchitectureContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<GetActiveOrderInfo> GetActiveOrder(long MemberID)
+        {
+            IQueryable<Object> Result = _dbContext.TransactionQueue.FromSql(
+                   @"Select TQ.TrnNo,TQ.TrnDate,TTQ.TrnType,TTQ.Order_Currency,TTQ.Delivery_Currency,
+                   CASE WHEN TTQ.BuyQty=0 THEN TTQ.SellQty WHEN TTQ.SellQty=0 THEN TTQ.BuyQty END as Amount,
+                   CASE WHEN TTQ.BidPrice=0 THEN TTQ.AskPrice WHEN TTQ.AskPrice=0 THEN TTQ.BidPrice END as Price,
+                   TTQ.IsCancelled from TransactionQueue TQ INNER JOIN TradeTransactionQueue TTQ ON TQ.TrnNo=TTQ.TrnNo 
+                   Where TQ.Status=4 AND TQ.TrnType IN(4,5) And TQ.MemberID=@MemberID", MemberID);
+            return null;
+        }
+
     }
 }
--- CleanArchitecture.Infrastructure/Services/Transaction/FrontTrnService.cs
+++ CleanArchitecture.Infrastructure/Services/Transaction/FrontTrnService.cs
@@ -1,4 +1,6 @@
 ﻿using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.Interfaces.Repository;
+using CleanArchitecture.Core.ViewModels.Transaction;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +9,15 @@ namespace CleanArchitecture.Infrastructure.Services.Transaction
 {
     public class FrontTrnService : IFrontTrnService
     {
+        private readonly IFrontTrnRepository _frontTrnRepository;
+        public FrontTrnService(IFrontTrnRepository frontTrnRepository)
+        {
+            _frontTrnRepository = frontTrnRepository;
+        }
+        public List<GetActiveOrderInfo> GetActiveOrder(long MemberID)
+        {
+            List<GetActiveOrderInfo> ActiveOrderList = _frontTrnRepository.GetActiveOrder(MemberID);
+            return ActiveOrderList; 
+        }
     }
 }
