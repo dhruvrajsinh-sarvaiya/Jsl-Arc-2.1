1a855be2ef964c8c5120470033402ac4e9b7e51e
vishvata <vishvata@jbspl.com>
2018-10-10 19:26:16 +0530

    Upadte Create wallet method

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/CreateWalletRequest.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/CreateWalletRequest.cs
@@ -7,18 +7,29 @@ namespace CleanArchitecture.Core.ViewModels.Wallet
 {
     public class CreateWalletRequest
     {
+        //vsolanki 10-10-2018 
         [Required]
-        public string label { get; set; }
+        public string WalletName { get; set; }
 
         [Required]
-        public string passphrase { get; set; }
+        public long OTP { get; set; }
 
-        public string userKey { get; set; }
-        public string backupXpub { get; set; }
-        public string backupXpubProvider { get; set; }
-        public string enterprise { get; set; }
-        public bool disableTransactionNotifications { get; set; }
-        public int gasPrice { get; set; }
-        public string passcodeEncryptionCode { get; set; }
+        public byte IsDefaultWallet { get; set; }
+
+        public int[] AllowTrnType { get; set; }
+
+        //[Required]
+        //public string label { get; set; }
+
+        //[Required]
+        //public string passphrase { get; set; }
+
+        //public string userKey { get; set; }
+        //public string backupXpub { get; set; }
+        //public string backupXpubProvider { get; set; }
+        //public string enterprise { get; set; }
+        //public bool disableTransactionNotifications { get; set; }
+        //public int gasPrice { get; set; }
+        //public string passcodeEncryptionCode { get; set; }
     }
 }
