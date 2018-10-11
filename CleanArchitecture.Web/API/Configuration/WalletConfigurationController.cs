0b643b94cffc342d1701752ff8d83d35c8ca3ad7
vishvata <vishvata@jbspl.com>
2018-10-11 12:18:04 +0530

    Add AddWalletTypeMaster API

--- CleanArchitecture.Web/API/Configuration/WalletConfigurationController.cs
+++ CleanArchitecture.Web/API/Configuration/WalletConfigurationController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Entities.User;
 using CleanArchitecture.Core.Interfaces.Configuration;
+using CleanArchitecture.Core.ViewModels.WalletConfiguration;
 using CleanArchitecture.Infrastructure.Services.Configuration;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,14 +18,26 @@ namespace CleanArchitecture.Web.API.Configuration
     public class WalletConfigurationController : Controller
     {
         private readonly IWalletConfigurationService _walletConfigurationService;
-        public WalletConfigurationController(IWalletConfigurationService walletConfigurationService)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public WalletConfigurationController(IWalletConfigurationService walletConfigurationService, UserManager<ApplicationUser> userManager)
         {
             _walletConfigurationService = walletConfigurationService;
-        }
-       [HttpGet]
+            _userManager=userManager;
+    }
+        [HttpGet]
         public IActionResult ListAllWalletTypeMaster()
         {
             var items = _walletConfigurationService.ListAllWalletTypeMaster();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalletTypeMasterAsync(AddWalletTypeMasterRequest addWalletTypeMasterRequest)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            long Userid= 8/*user.Id*/;
+            var items = _walletConfigurationService.AddWalletTypeMaster(addWalletTypeMasterRequest, Userid);
             return Ok(items);
         }
     }
