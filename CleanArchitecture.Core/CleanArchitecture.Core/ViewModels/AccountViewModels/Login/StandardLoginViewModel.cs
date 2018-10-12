d477413b2ce027d4a0c9e634e04b31e3e565437c
niravjbspl <niravs@jbspl.con>
2018-10-12 16:55:26 +0530

    response changes in standard login view model

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/StandardLoginViewModel.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/StandardLoginViewModel.cs
@@ -9,15 +9,16 @@ namespace CleanArchitecture.Core.ViewModels.AccountViewModels.Login
 {
     public class StandardLoginViewModel : TrackerViewModel
     {
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "1,Please enter a User Name,4001")]
+        [Display(Name = "Username")]
+        [StringLength(50, ErrorMessage = "1,Please enter a valid User Name,4002")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "1,Please Enter Password,4010")]
         [DataType(DataType.Password)]
-        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(50, ErrorMessage = "1,The {0} must be at least {2} and at max {1} characters long,4011", MinimumLength = 6)]
         [Display(Name = "Password")]
-        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$", ErrorMessage = "Passwords must be at least 6 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$", ErrorMessage = "1,Passwords must be at least 6 characters and contain at 3 of 4 of the following: upper case (A-Z) lower case (a-z) number (0-9) and special character (e.g. !@#$%^&*),4028")]
         public string Password { get; set; }
 
        // public bool RememberMe { get; set; }
