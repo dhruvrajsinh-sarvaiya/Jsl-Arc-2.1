77b7a2f12869003161a81fe934986d8150ac3e49
vishvata <vishvata@jbspl.com>
2018-10-12 19:09:43 +0530

    Add validation

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/WalletConfiguration/WalletTypeMasterRequest.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/WalletConfiguration/WalletTypeMasterRequest.cs
@@ -8,21 +8,21 @@ namespace CleanArchitecture.Core.ViewModels.WalletConfiguration
 {
     public class WalletTypeMasterRequest 
     {
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "1,Please Enter parameters,4029")]
+        [StringLength(50, ErrorMessage = "1,Please enter a valid parameters,4030")]
         public string WalletTypeName { get; set; }
 
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "1,Please Enter parameters,4029")]
+        [StringLength(100, ErrorMessage = "1,Please enter a valid parameters,4030")]
         public string Discription { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "1,Please Enter parameters,4029")]
         public short IsDepositionAllow { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "1,Please Enter parameters,4029")]
         public short IsWithdrawalAllow { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "1,Please Enter parameters,4029")]
         public short IsTransactionWallet { get; set; }
 
         public short Status { get; set; }
