546847376693d67f7bc25c033bcc58d8c415c3df
vishvata <vishvata@jbspl.com>
2018-10-13 12:45:07 +0530

    Add new entity for wallet

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/Entities/Wallet/WalletTransactionOrder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.Entities.Wallet
+{
+    public class WalletTransactionOrder: SharedKernel.BizBase
+    {
+        [Required]
+        public long OrderID { get; set; }
+
+        [Required]
+        public DateTime TrnDate { get; set; }
+
+        [Required]
+        public long OWalletID { get; set; }
+
+        [Required]
+        public long DWalletID { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string WalletType { get; set; }
+
+        [Required]
+        public long OTrnNo { get; set; }
+
+        [Required]
+        public long DTrnNo { get; set; }
+       // public string Status { get; set; }
+        public string StatusMsg { get; set; }
+    }
+}
---
+++ CleanArchitecture.Core/CleanArchitecture.Core/Entities/Wallet/WalletTransactionQueue.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.Entities.Wallet
+{
+    public class WalletTransactionQueue: BizBase
+    {
+        [Required]
+        public long TrnNo { get; set; }
+
+        [Required]
+        public string Guid { get; set; }
+
+        [Required]
+        public byte TrnType { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public long TradeID { get; set; }
+
+        [Required]
+        public DateTime TrnDate { get; set; }
+
+        [Required]
+        public long WalletID { get; set; }
+
+        [Required]
+        public string WalletType { get; set; }
+
+        [Required]
+        public long MemberID { get; set; }
+
+        public string TimeStamp { get; set; }
+      //  public byte Status { get; set; }
+        public string StatusMsg { get; set; }
+    }
+}
