f582f24125f665caff12057c7730e3571d999a5d
Nupoora <43128628+Nupoora@users.noreply.github.com>
2018-10-15 10:49:27 +0530

    class added

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ApiModels/CreditWalletDrArryTrnID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.ApiModels
+{
+    public class CreditWalletDrArryTrnID
+    {
+        public string DrTrnRefNo { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
