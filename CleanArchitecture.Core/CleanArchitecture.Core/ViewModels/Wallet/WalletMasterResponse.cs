cb9e62f839961b4b953a6db3645a806eead573cb
vishvata <vishvata@jbspl.com>
2018-10-16 19:10:24 +0530

    Add class

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/WalletMasterResponse.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/WalletMasterResponse.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace CleanArchitecture.Core.Entities.Wallet
+namespace CleanArchitecture.Core.ViewModels.Wallet
 {
     public class WalletMasterResponse
     {
         public string Walletname { get; set; }
         public decimal Balance { get; set; }
         public string CoinName { get; set; }
-        public string AccWalletID { get; set; }  
+        public string AccWalletID { get; set; }
         public string PublicAddress { get; set; }
         public byte IsDefaultWallet { get; set; }
     }
