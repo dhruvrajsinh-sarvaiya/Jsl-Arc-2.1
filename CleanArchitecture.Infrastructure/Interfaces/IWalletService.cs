820c59d91971c1af0eb65d759d0dc044d692e355
Rushabh <rhp_jbspl@outlook.com>
2018-10-16 10:10:15 +0530

    Conflict Resolved

--- CleanArchitecture.Infrastructure/Interfaces/IWalletService.cs
+++ CleanArchitecture.Infrastructure/Interfaces/IWalletService.cs
@@ -37,47 +37,6 @@ namespace CleanArchitecture.Infrastructure.Interfaces
         ListWalletResponse GetWalletById(long userid, string coin,string walletId);
 
         BizResponseClass GetWalletDeductionNew(string coinName, string timestamp, byte orderType, decimal amount, long userID, string accWalletID, long TrnRefNo, enServiceType serviceType, enWalletTrnType trnType);
-        
-        
-    }
-}
-using CleanArchitecture.Core.ApiModels;
-using CleanArchitecture.Core.Entities;
-using CleanArchitecture.Core.Enums;
-using CleanArchitecture.Core.SharedKernel;
-using CleanArchitecture.Core.ViewModels.Wallet;
-using CleanArchitecture.Core.ViewModels.WalletOperations;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CleanArchitecture.Infrastructure.Interfaces
-{
-    
-    public interface IWalletService
-    {
-        decimal GetUserBalance(long walletId);
-
-        bool WalletBalanceCheck(decimal amount, long walletId);
-
-        bool IsValidWallet(long walletId);
-
-        CreateWalletAddressRes GenerateAddress(string walletid,string coin);
-
-        //vsolanki 8-10-2018
-        IEnumerable<WalletTypeMaster> GetWalletTypeMaster();
-
-        //vsolanki 10-10-2018
-        CreateWalletResponse InsertIntoWalletMaster(string Walletname, string CoinName, byte IsDefaultWallet, int[] AllowTrnType, long userId);
-
-        //ntrivedi 11-10-2018
-        BizResponseClass DebitBalance(long userID, long WalletID, decimal amount, int walletTypeID, enWalletTrnType wtrnType, enTrnType trnType, enServiceType serviceType, long trnNo, string smsCode);
-
-        ListWalletResponse ListWallet(long userid);
-
-        ListWalletResponse GetWalletByCoin(long userid, string coin);
-
-        ListWalletResponse GetWalletById(long userid, string coin,string walletId);
 
         ListWalletAddressResponse ListAddress(string AccWalletID);
 
