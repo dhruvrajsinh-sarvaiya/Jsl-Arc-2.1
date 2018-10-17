eb1204ca104fc7834d2487208c21b1d46e007ef2
birjujbspl <birju@jbspl.com>
2018-10-17 14:55:58 +0530

    Update CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Log/IpMasterViewModel.cs

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Log/IpMasterViewModel.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Log/IpMasterViewModel.cs
@@ -39,5 +39,8 @@ namespace CleanArchitecture.Core.ViewModels.AccountViewModels.Log
 
     public class IpAddressResponse : BizResponseClass
     {
+        public int TotalRow { get; set; }
+        public List<IpMasterViewModel> IpList { get; set; }
+
     }
 }
