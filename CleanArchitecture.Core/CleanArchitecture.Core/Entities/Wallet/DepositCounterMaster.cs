51898963f7e57c326df55390b6c7e68b444388e9
Nupoora <43128628+Nupoora@users.noreply.github.com>
2018-10-22 14:51:20 +0530

    composite key added

--- CleanArchitecture.Core/CleanArchitecture.Core/Entities/Wallet/DepositCounterMaster.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/Entities/Wallet/DepositCounterMaster.cs
@@ -1,17 +1,24 @@
 ﻿using CleanArchitecture.Core.SharedKernel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CleanArchitecture.Core.Entities.Wallet
 {
     public class DepositCounterMaster: BizBase
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public new long Id { get; set; }
+
         public int RecordCount { get; set; }
         public long Limit { get; set; }
         public string LastTrnID { get; set; }
         public long MaxLimit { get; set; }
+        [Key]
         public long WalletTypeID { get; set; }
+        [Key]
         public long SerProId { get; set; }
         public string PreviousTrnID { get; set; }
         public long TPSPickupStatus { get; set; }
