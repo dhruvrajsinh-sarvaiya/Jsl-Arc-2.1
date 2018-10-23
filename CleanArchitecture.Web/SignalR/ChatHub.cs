61246640f7f4284a512ee065f62b064b144cf040
khushali_JBSPL <khushali@jbspl.com>
2018-10-23 11:33:08 +0530

    Add Transaction history , Setteled order , Pending order update method

--- CleanArchitecture.Web/SignalR/ChatHub.cs
+++ CleanArchitecture.Web/SignalR/ChatHub.cs
@@ -10,82 +10,117 @@ namespace CleanArchitecture.Web.SignalR
 {
     public class Chat : Hub
     {
-        //private static IOptions<RedisConfiguration> redis;
         private RedisConnectionFactory _fact;
-        //= new RedisConnectionFactory(redis);
         public Chat(RedisConnectionFactory factory)
         {
             _fact = factory;
         }
+
+        //For Testing Connection
         public Task Send(string message)
         {
             return Clients.All.SendAsync("Send", message);
         }
-        public void SendChatMessage(long UserID, string message)
-        {
-            // string name = Context.User.Identity.Name;
-            var Redis = new RadisServices<ConnetedClientList>(this._fact);
-            ConnetedClientList User = new ConnetedClientList();
-            User = Redis.GetConnectionID(UserID + "ConnectionID");
-            //foreach (var connectionId in _connections.GetConnections(who))
-            //{
-            Clients.Client(User.ConnectionId).SendAsync("RecieveMessage", User + ": " + message);
-            Clients.Group("BroadCast").SendAsync("BroadcastMessage", User + ": " + message);
-            // Clients.All.SendAsync("BroadcastMessage", token + ": " + message);
-            //}
-        }
 
-        public void SendGroupMessage(string name, string message)
-        {
-            // Call the broadcastMessage method to update clients.
-            Clients.Group("GroupMessage").SendAsync("ReciveGroupMessage", name, message);
-            var Redis = new RadisServices<ChatHistory>(this._fact);
-            Redis.SaveToSet("GroupChatHistory", new ChatHistory { Name = name, Message = message, Id = Guid.NewGuid() }, name);
-            Redis.GetSetData("GroupChatHistory");
-        }
-
-        public void BlockedUsers(string id)
-        {
-            // Call the broadcastMessage method to update clients.
-            //Clients.Group("GroupMessage").SendAsync("ReciveGroupMessage", name, message);
-            //var redis = new RadisServices<ChatHistory>(this._fact);
-            //redis.SaveToSet("GroupChatHistory", new ChatHistory { Name = name, Message = message, Id = Guid.NewGuid() }, name);
-            //redis.GetSetData("GroupChatHistory");
-        }
-
+        // Add for Subscription Channel
         public override Task OnConnectedAsync()
         {
-            //string name = Context.User.Identity.Name;
-
-            //_connections.Add(name, Context.ConnectionId);
             Groups.AddToGroupAsync(Context.ConnectionId, "BroadCast").Wait();
             Groups.AddToGroupAsync(Context.ConnectionId, "GroupMessage").Wait();
             return base.OnConnectedAsync();
         }
 
-        public void OnConnected(long  UserID,string AccessToken,string Username)
+        public void OnConnected(string UserID, string AccessToken, string Username)
         {
             var Redis = new RadisServices<ConnetedClientList>(this._fact);
-            //_connections.Add(token, Context.ConnectionId);
-            Redis.SaveToHash(UserID + "ConnectionID", new ConnetedClientList { ConnectionId = Context.ConnectionId }, Context.ConnectionId);
+            Redis.SaveToHash(UserID + ":ConnectionDetail", new ConnetedClientList { ConnectionId = Context.ConnectionId }, Context.ConnectionId);
             var Redis1 = new RadisServices<ConnetedClientToken>(this._fact);
-            Redis1.SaveToHash(UserID + "Token", new ConnetedClientToken { Token  = AccessToken }, Context.ConnectionId);
+            Redis1.SaveToHash(UserID + ":Token", new ConnetedClientToken { Token = AccessToken }, Context.ConnectionId);
             var Redis2 = new RadisServices<ConnetedUserDetail>(this._fact);
-            Redis2.SaveToHash(UserID + "Username", new ConnetedUserDetail { UserName = Username }, Context.ConnectionId);
+            Redis2.SaveToHash(UserID + ":UserDetail", new ConnetedUserDetail { UserName = Username }, Context.ConnectionId);
 
             //Groups.AddToGroupAsync(Context.ConnectionId, "BroadCast");
         }
 
         public override Task OnDisconnectedAsync(System.Exception exception)
         {
-            //string name = Context.User.Identity.Name;
-            var redis = new RadisServices<ConnetedClientList>(this._fact);
-            redis.scan(Context.ConnectionId);
-            //_connections.Remove(name, Context.ConnectionId);
+            var Redis = new RadisServices<ConnetedClientList>(this._fact);
+            Redis.Scan(Context.ConnectionId, ":ConnectionDetail");
             Groups.RemoveFromGroupAsync(Context.ConnectionId, "BroadCast").Wait();
             Groups.RemoveFromGroupAsync(Context.ConnectionId, "GroupMessage").Wait();
             return base.OnDisconnectedAsync(exception);
         }
+
+        // Remove From Subscription Channel
+        public void RemoveSubscription(string ChannelName)
+        {
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, ChannelName).Wait();
+        }
+
+        // Add to Subscription Channel
+        public void AddSubscription(string ChannelName)
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, ChannelName).Wait();
+        }
+
+        // one to one Message Chat
+        public void SendChatMessage(string UserID, string message)
+        {
+            var Redis = new RadisServices<ConnetedClientList>(this._fact);
+            ConnetedClientList User = new ConnetedClientList();
+            User = Redis.GetConnectionID(UserID + ":ConnectionDetail");
+            Clients.Client(User.ConnectionId).SendAsync("RecieveMessage", User + ": " + message);
+            //Clients.Group("BroadCast").SendAsync("BroadcastMessage", User + ": " + message);
+        }
+
+        public void PendingOrderUpdate(string UserID, string Data)
+        {
+            var Redis = new RadisServices<ConnetedClientList>(this._fact);
+            ConnetedClientList User = new ConnetedClientList();
+            User = Redis.GetConnectionID(UserID + ":ConnectionDetail");
+            Clients.Client(User.ConnectionId).SendAsync("RecievePendingOrderUpdate", Data);
+            //Clients.Group("BroadCast").SendAsync("BroadcastMessage", Data);
+        }
+
+        public void SattledOrderUpdate(string Data)
+        {
+            Clients.Group("BroadCast").SendAsync("BroadcastMessage", Data);
+        }
+
+
+        public void TransactionHistory(string Data)
+        {
+            Clients.Group("BroadCast").SendAsync("BroadcastMessage", Data);
+        }
+
+        public void SendGroupMessage(string Name, string Message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Group("GroupMessage").SendAsync("ReciveGroupMessage", Name, Message);
+            var Redis = new RadisServices<ChatHistory>(this._fact);
+            Redis.SaveToSet("GroupChatHistory", new ChatHistory { Name = Name, Message = Message, Id = Guid.NewGuid() }, Name);
+            Redis.GetSetData("GroupChatHistory");
+        }
+
+        public void GetChatHistory(string Name, string Message)
+        {
+            var Redis = new RadisServices<ChatHistory>(this._fact);
+            string Data = Redis.GetSetData("GroupChatHistory");
+            Clients.Client(Context.ConnectionId).SendAsync("RecieveChatHistory", Data);
+        }
+
+        public void BlockedUsers(string UserID)
+        {
+            // Add to blocklist
+            var Redis2 = new RadisServices<BlockUserDetail>(this._fact);
+            Redis2.SaveToHash(UserID + ":BlockDetail", new BlockUserDetail { IsBlock = true }, Context.ConnectionId);
+
+            // Call the broadcastMessage method to update clients.
+            //Clients.Group("GroupMessage").SendAsync("ReciveGroupMessage", name, message);
+            //var redis = new RadisServices<ChatHistory>(this._fact);
+            //redis.SaveToSet("GroupChatHistory", new ChatHistory { Name = name, Message = message, Id = Guid.NewGuid() }, name);
+            //redis.GetSetData("GroupChatHistory");
+        }       
 
     }
 }
