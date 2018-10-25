0101b548e88f52c1a61d9329cffd097e1af08bd6
komal siddhapura <komal@jbspl.com>
2018-10-25 11:22:24 +0530

    Tradding summary class added

--- CleanArchitecture.Core/CleanArchitecture.Core/ApiModels/TransactionApiConfiguration.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ApiModels/TransactionApiConfiguration.cs
@@ -106,4 +106,20 @@ namespace CleanArchitecture.Core.ApiModels
         public decimal TodayClose { get; set; }
         public decimal TodayOpen { get; set; }
     }
+    public class GetTradingSummary
+    {
+        public long TrnNo { get; set; }
+        public long MemberID { get; set; }
+        public string Type { get; set; }
+        public Decimal Price { get; set; }
+        public Decimal Amount { get; set; }
+        public Decimal Total { get; set; }
+        public DateTime DateTime { get; set; }
+        public string StatusText { get; set; }
+        public long PairID { get; set; }
+        public String PairName { get; set; }
+        public Decimal ChargeRs { get; set; }
+        public Decimal PreBal { get; set; }
+        public Decimal PostBal { get; set; }
+    }
 }
