15299a3698b12f0d003a4028096a1a87bb6fec64
Pankaj <pankaj@jbspl.com>
2018-10-25 16:20:29 +0530

    Social login viewmodel add error message

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/SocialLoginWithGoogleViewModel.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/SocialLoginWithGoogleViewModel.cs
@@ -6,7 +6,8 @@ namespace CleanArchitecture.Core.ViewModels.AccountViewModels.Login
 {
     public class SocialLoginWithGoogleViewModel : TrackerViewModel
     {
-        [Required]
+        
+        [Required(ErrorMessage = "1,Please Enter Email Id,4007")]
         [EmailAddress]
         [RegularExpression(@"^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$", ErrorMessage = "Please enter a valid Email Address")]
         public string Email { get; set; }
