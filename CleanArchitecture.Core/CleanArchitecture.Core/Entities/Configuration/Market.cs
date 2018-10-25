98c8cb8b847b67897a02a266a9acd80ba9d049a1
komal siddhapura <komal@jbspl.com>
2018-10-25 11:03:49 +0530

    Add New Market Entity

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/Entities/Configuration/Market.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.Entities.Communication
+{
+    public class Market : BizBase
+    {
+        public string CurrencyName { get; set; }
+        public short isBaseCurrency { get; set; }
+    }
+}
