d28499d0c7eb4ff4f08a4bacff02189787d623e0
Pankaj <pankaj@jbspl.com>
2018-10-25 16:38:16 +0530

    Update CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/SocialLoginWithGoogleViewModel.cs

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/SocialLoginWithGoogleViewModel.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Login/SocialLoginWithGoogleViewModel.cs
@@ -11,11 +11,11 @@ namespace CleanArchitecture.Core.ViewModels.AccountViewModels.Login
         [EmailAddress]
         [RegularExpression(@"^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$", ErrorMessage = "Please enter a valid Email Address")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "2,Please Enter ProviderKey,4092")]
         public string ProviderKey { get; set; }
-        [Required]
+        [Required(ErrorMessage = "3,Please Enter ProviderName,4092")]
         public string ProviderName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "4,Please Enter access_token,4094")]
         public string access_token { get; set; }
         //public string refresh_token { get; set; }
         //public string token_type { get; set; }
