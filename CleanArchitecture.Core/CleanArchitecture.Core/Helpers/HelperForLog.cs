b123ad0247507ff3883782efb5ccdc11dd2f0df9
RitaChavda <rita@jbspl.com>
2018-10-26 11:13:34 +0530

    remove unused in log class

--- CleanArchitecture.Core/CleanArchitecture.Core/Helpers/HelperForLog.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/Helpers/HelperForLog.cs
@@ -21,7 +21,7 @@ namespace CleanArchitecture.Core.Helpers
                 logger.Error(ex);
             }
         }
-        public static void WriteErrorLog(string MethodName, string Controllername, string Error)
+        public static void WriteErrorLog(string MethodName, string Controllername, string Error, string accessToken = null)
         {
             var logger = NLog.LogManager.GetCurrentClassLogger();
             try
