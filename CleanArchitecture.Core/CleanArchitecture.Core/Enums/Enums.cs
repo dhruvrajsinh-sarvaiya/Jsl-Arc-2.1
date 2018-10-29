5709fb4918aa998fcf1a3ae86e11db7e9bb63b8b
Rushabh <rhp_jbspl@outlook.com>
2018-10-29 15:51:22 +0530

    Added Common Function For Shadow Balance Check

--- CleanArchitecture.Core/CleanArchitecture.Core/Enums/Enums.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/Enums/Enums.cs
@@ -278,7 +278,8 @@ namespace CleanArchitecture.Core.Enums
         //NotFoundLimit=4234,
         //DuplicateRecord=4235,       
         InvalidAddress = 4236,
-        OrgIDNotFound = 2427
+        OrgIDNotFound = 2427,
+        MemberTypeNotFound = 4242
     }
     //Rushabh 05-10-2018 as per instruction by nupoora mam change Enum 0 for Success and 1 for Fail
     public enum enResponseCodeService
