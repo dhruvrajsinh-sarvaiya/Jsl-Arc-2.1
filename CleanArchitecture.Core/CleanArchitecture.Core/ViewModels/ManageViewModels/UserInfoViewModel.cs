cd26b3b756b69d0abd078ff32ba8ce0491b3c7e5
Pankaj <pankaj@jbspl.com>
2018-10-29 16:09:22 +0530

    Update CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/ManageViewModels/UserInfoViewModel.cs

--- CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/ManageViewModels/UserInfoViewModel.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/ManageViewModels/UserInfoViewModel.cs
@@ -10,6 +10,7 @@ namespace CleanArchitecture.Core.ViewModels.ManageViewModels
         public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+
         public string Mobile { get; set; }
 
     }
