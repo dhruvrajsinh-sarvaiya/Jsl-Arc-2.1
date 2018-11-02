91276809714aedfde1b72ed6f0df08fb60ccc45a
niravjbspl <niravs@jbspl.con>
2018-11-02 19:05:32 +0530

    add ip history service,Interface and view model

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/AccountViewModels/Log/IpHistoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Core.ViewModels.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels.AccountViewModels.Log
+{
+    public class IpHistoryViewModel
+    {
+        public long Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string IpAddress { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Location { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
