712d1f74187d407203491e48efb4e8be58ff8f84
Rushabh <rhp_jbspl@outlook.com>
2018-11-02 11:53:07 +0530

    Added New Class For TransferInOut Response

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/WalletOperations/TransferInOutRes.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Core.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels.WalletOperations
+{
+    public class TransferInOutRes
+    {
+        public List<TransfersRes> Transfers { get; set; }
+        public BizResponseClass BizResponseObj { get; set; }
+    }
+    public class TransfersRes
+    {
+        public long AutoNo { get; set; }
+
+        public string TrnID { get; set; }
+
+        public string WalletType { get; set; }
+
+        public string Address { get; set; }
+
+        public long Confirmations { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string ConfirmedTime { get; set; }
+
+        public short? ConfirmationCount { get; set; }
+    }
+}
