c696b3ca6ac578af2e283aa6097f47636c148e85
birjujbspl <birju@jbspl.com>
2018-11-02 11:09:16 +0530

    Revert "userkeymaster service"
    This reverts commit 2fc334a546d778c70bed793b2e0e023c75fbbd68.

--- CleanArchitecture.Core/CleanArchitecture.Core/Entities/User/UserKeyMaster.cs
+++ CleanArchitecture.Core/CleanArchitecture.Core/Entities/User/UserKeyMaster.cs
@@ -16,17 +16,17 @@ namespace CleanArchitecture.Core.Entities.User
 
         public bool EnableStatus { get; set; }
 
-        public void SetAsUniqueKeyStatus()
+        public void SetAsPasswordStatus()
         {
             EnableStatus = true;
             Events.Add(new ServiceStatusEvent<UserKeyMaster>(this));
         }
 
-        public void SetAsUpdateDate(long Id)
-        {
-            UpdatedDate = DateTime.UtcNow;
-            UpdatedBy = Id;
-            Events.Add(new ServiceStatusEvent<UserKeyMaster>(this));
-        }
+        //public void SetAsUpdateDate(long Id)
+        //{
+        //    UpdatedDate = DateTime.UtcNow;
+        //    UpdatedBy = Id;
+        //    Events.Add(new ServiceStatusEvent<CustomPassword>(this));
+        //}
     }
 }
