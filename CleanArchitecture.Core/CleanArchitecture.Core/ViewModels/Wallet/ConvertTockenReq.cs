2285aa1572de834b8bfde571cf191dad29366127
vishvata <vishvata@jbspl.com>
2018-11-02 19:34:11 +0530

    add class

---
+++ CleanArchitecture.Core/CleanArchitecture.Core/ViewModels/Wallet/ConvertTockenReq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.ViewModels.Wallet
+{
+    public class ConvertTockenReq
+    {
+        public string SourceCoin { get; set; }
+        public string DestinationCoin { get; set; }
+        public decimal SourcePrice { get; set; }
+        public string DestinationPrice { get; set; }
+    }
+}
