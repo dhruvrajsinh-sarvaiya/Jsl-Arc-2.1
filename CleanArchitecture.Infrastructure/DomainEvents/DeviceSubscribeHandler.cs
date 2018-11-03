60a5683d6ace57dc3951184664b69f97a9bf8e0e
khushali_JBSPL <khushali@jbspl.com>
2018-11-03 16:28:54 +0530

    Added subscibe method for push notification

--- CleanArchitecture.Infrastructure/DomainEvents/DeviceSubscribeHandler.cs
+++ CleanArchitecture.Infrastructure/DomainEvents/DeviceSubscribeHandler.cs
@@ -28,6 +28,37 @@ namespace CleanArchitecture.Infrastructure.Services
         {
             try
             {
+                CommunicationResponse Response = new CommunicationResponse();
+                if (Request.SubsscrptionType == EnDeviceSubsscrptionType.Subsscribe)
+                {
+                    Response = await SubscibePushNotification(Request);
+                }
+                else if (Request.SubsscrptionType == EnDeviceSubsscrptionType.UnSubsscribe)
+                {
+                    Response = await SubscibePushNotification(Request);
+                }
+                return await Task.FromResult(new CommunicationResponse
+                {
+                    ReturnCode = enResponseCode.Fail,
+                    ReturnMsg = EnResponseMessage.PushNotificationSubscriptionFail,
+                    ErrorCode = enErrorCode.PushNotificationSubscriptionFail
+                });
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(new CommunicationResponse
+                {
+                    ReturnCode = enResponseCode.InternalError,
+                    ReturnMsg = EnResponseMessage.PushNotificationSubscriptionFail,
+                    ErrorCode = enErrorCode.PushNotificationSubscriptionFail
+                });
+            }
+        }
+
+        public async Task<CommunicationResponse> SubscibePushNotification(DeviceRegistrationRequest Request)
+        {
+            try
+            {
                 var DeviceStore = new DeviceStore();
                 DeviceStore = _MessageRepository.GetById(Request.UserID);
                 if (DeviceStore.UserID > 0)
@@ -46,33 +77,26 @@ namespace CleanArchitecture.Infrastructure.Services
                         CreatedDate = DateTime.UtcNow
                     };
                     _MessageRepository.Add(DeviceStore);
-                }                
-                return await Task.FromResult(new CommunicationResponse { ReturnCode = enResponseCode.Success,
-                ReturnMsg = EnResponseMessage.PushNotificationSubscriptionSuccess,
-                ErrorCode = enErrorCode.PushNotificationSubscriptionSuccess
-            });
+                }
+                return await Task.FromResult(new CommunicationResponse
+                {
+                    ReturnCode = enResponseCode.Success,
+                    ReturnMsg = EnResponseMessage.PushNotificationSubscriptionSuccess,
+                    ErrorCode = enErrorCode.PushNotificationSubscriptionSuccess
+                });
             }
             catch (Exception ex)
             {
-                return await Task.FromResult(new CommunicationResponse {
+                return await Task.FromResult(new CommunicationResponse
+                {
                     ReturnCode = enResponseCode.InternalError,
                     ReturnMsg = EnResponseMessage.PushNotificationSubscriptionFail,
                     ErrorCode = enErrorCode.PushNotificationSubscriptionFail
                 });
-            }            
-        }
-    }
-
-    public class DeviceUnsubscribeHandler : IRequestHandler<DeviceRegistrationRequest, CommunicationResponse>
-    {
-        private readonly IMessageRepository<DeviceStore> _MessageRepository;
-
-        public DeviceUnsubscribeHandler(IMessageRepository<DeviceStore> MessageRepository)
-        {
-            _MessageRepository = MessageRepository;
+            }
         }
 
-        public async Task<CommunicationResponse> Handle(DeviceRegistrationRequest Request, CancellationToken cancellationToken)
+        public async Task<CommunicationResponse> UnsbscibePushNotification(DeviceRegistrationRequest Request)
         {
             try
             {
