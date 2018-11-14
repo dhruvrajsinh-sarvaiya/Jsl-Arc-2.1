d6a1e4aa3c5eafeeb6bd3e2cc210b932fff26962
RitaChavda <rita@jbspl.com>
2018-11-14 10:26:59 +0530

    bidprice find issue solve in settlement

--- CleanArchitecture.Infrastructure/Data/Transaction/SettlementRepository.cs
+++ CleanArchitecture.Infrastructure/Data/Transaction/SettlementRepository.cs
@@ -187,13 +187,25 @@ namespace CleanArchitecture.Infrastructure.Data.Transaction
                 TradeBuyerListObj.IsProcessing = 1;
                 _TradeBuyerList.Update(TradeBuyerListObj);
 
-               
+                IEnumerable<TradeSellerList> MatchSellerListBase;
                 //SortedList<TradeSellerList, TradeSellerList>
-                var MatchSellerListBase = _TradeSellerList.FindBy(item => item.Price <= TradeBuyRequestObj.BidPrice && item.IsProcessing == 0
-                                                        && item.BuyServiceID == TradeBuyRequestObj.PaidServiceID &&
-                                                        item.SellServiceID == TradeBuyRequestObj.ServiceID
-                                                        && (item.Status == Convert.ToInt16(enTransactionStatus.Initialize) || item.Status == Convert.ToInt16(enTransactionStatus.Hold))
-                                                        && item.RemainQty > 0);//Pending after partial Qty remain
+                if (TradeTransactionQueueObj.TrnType == Convert.ToInt16(enTrnType.Buy_Trade))//Take price as Highest and find lower
+                {
+                   MatchSellerListBase = _TradeSellerList.FindBy(item => item.Price <= TradeBuyRequestObj.BidPrice && item.IsProcessing == 0
+                                                       && item.BuyServiceID == TradeBuyRequestObj.PaidServiceID &&
+                                                       item.SellServiceID == TradeBuyRequestObj.ServiceID
+                                                       && (item.Status == Convert.ToInt16(enTransactionStatus.Initialize) || item.Status == Convert.ToInt16(enTransactionStatus.Hold))
+                                                       && item.RemainQty > 0);//Pending after partial Qty remain
+                }
+                else //Take price as Highest and find larger
+                {
+                    MatchSellerListBase = _TradeSellerList.FindBy(item => item.Price >= TradeBuyRequestObj.BidPrice && item.IsProcessing == 0
+                                                       && item.BuyServiceID == TradeBuyRequestObj.PaidServiceID &&
+                                                       item.SellServiceID == TradeBuyRequestObj.ServiceID
+                                                       && (item.Status == Convert.ToInt16(enTransactionStatus.Initialize) || item.Status == Convert.ToInt16(enTransactionStatus.Hold))
+                                                       && item.RemainQty > 0);//Pending after partial Qty remain
+                }
+                   
 
                 var MatchSellerList = MatchSellerListBase.OrderBy(x => x.Price).OrderBy(x => x.TrnNo);
 
